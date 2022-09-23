3527be115be0f07fcc134e0bd4a367c96e783397
SardarBadar <dataentrybadar@gmail.com>
2022-09-23 16:59:05 +0500

    added tailwind support

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div >
-      Ticktock here
-    </div>
+
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
