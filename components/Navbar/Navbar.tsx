dc455f1117b8b745c06c118cb21f53b6e02567a0
SardarBadar <dataentrybadar@gmail.com>
2022-09-23 19:06:38 +0500

    fixes

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../utils/tiktik-logo.png";
+const Navbar = () => {
+  return (
+    <div className="w-full justify-between flex items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href={"/"}>
+        <div className="w-[200px] md:w-[130px] ">
+          <Image className="cursor-pointer" src={Logo} alt="tiktik" layout="responsive"/>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-const Navbar = ()=>{
-    return <div className="h-[8vh]">nav i</div>
-
-}
-
-export default Navbar
+export default Navbar;
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {ImCancelCircle} from 'react-icons/im'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const Sidebar = () => {
+const [showSidebar, setShowSidebar] = useState(true);
   return (
-    <div>Sidebar</div>
+    <div>
+
+      <div className='block xl:hidden m-2 ml-4 mt-3 text-xl'>
+        {showSidebar?<ImCancelCircle onClick={()=>setShowSidebar((state)=>!state)}  />:<AiOutlineMenu onClick={()=>setShowSidebar((state)=>!state)} />}
+      </div>
+    </div>
   )
 }
 
