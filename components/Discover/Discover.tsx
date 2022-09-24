d7d99a2cecd15e54741fb351f9a15497ca0870e8
SardarBadar <dataentrybadar@gmail.com>
2022-09-24 17:17:13 +0500

    some bug fixes

--- components/Discover/Discover.tsx
+++ components/Discover/Discover.tsx
@@ -10,17 +10,17 @@ const Discover = () => {
 
    let topicStyles='xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black'
   return (
-    <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
+    <div className='xl:border-b-2 xl:border-gray-200 pb-4'>
 
-        <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>
+        <p className='text-gray-500 font-semibold m-2 mt-1 hidden xl:block'>
             Popular Topics
         </p>
-        <div className='flex gap-3 flex-wrap'>
+        <div className='flex gap-1 flex-wrap'>
             {
             topics.map((topics)=>{
                 return <Link href={`/?topic=${topics.name}`} key={topics.name}>
                     <div className={topic==topics.name?activeTopicStyle:topicStyles}>
-                        <span className='font-bold text-2xl xl:text-md'>
+                        <span className='font-bold text-xl xl:text-sm'>
                             {topics.icon}
                         </span>
                         <span className='font-medium text-md hidden xl:block'>
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { footerList1, footerList2, footerList3 } from "../../utils/constants";
 const Footer = () => {
-  const List: React.FC<{ footerList: string[] }> = ({ footerList }) => {
+  const List: React.FC<{ footerList: string[],mt:boolean }> = ({ footerList,mt }) => {
     return (
-      <>
+      <div className={`flex flex-wrap gap-2 ${mt && 'mt-3'}`}>
         {footerList.map((fl) => (
           <p
             key={fl}
-            className="cursor-pointer text-gray-400 text-sm hover:underline"
+            className="overflow-x-hidden cursor-pointer text-gray-400 text-sm hover:underline"
           >
             {fl}
           </p>
         ))}
-      </>
+      </div>
     );
   };
   return (
-    <div className="mt-6 hidden xl:block">
-      <div className="flex flex-wrap gap-2 mt-5">
-        <List footerList={footerList1} />
-        <List footerList={footerList2} />
-        <List footerList={footerList3} />
+    <div className="mt-4 hidden xl:block">
+      { <div className="flex flex-wrap gap-1 mt-2 overflow-x-hidden"> 
+        <List footerList={footerList1} mt={false}/>
+        <List footerList={footerList2} mt={false}/>
+       {<List footerList={footerList3} mt={false}/> }
+        <p className="text-gray-400 text-sm
+        mt-3">tiktik 2022 by Badar</p>
        
-      </div>
+      </div> }
     </div>
   );
 };
