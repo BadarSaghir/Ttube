ff9c7aff0d11474bb82cb0b3b82864608e45402a
SardarBadar <dataentrybadar@gmail.com>
2022-09-24 16:54:48 +0500

    footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { footerList1, footerList2, footerList3 } from "../../utils/constants";
 const Footer = () => {
+  const List: React.FC<{ footerList: string[] }> = ({ footerList }) => {
+    return (
+      <>
+        {footerList.map((fl) => (
+          <p
+            key={fl}
+            className="cursor-pointer text-gray-400 text-sm hover:underline"
+          >
+            {fl}
+          </p>
+        ))}
+      </>
+    );
+  };
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-6 hidden xl:block">
+      <div className="flex flex-wrap gap-2 mt-5">
+        <List footerList={footerList1} />
+        <List footerList={footerList2} />
+        <List footerList={footerList3} />
+       
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
